42b523b93be2c2b857715645ff825fbea4da1283
keisymachado <keisymachado26@gmail.com>
2025-05-01 23:53:02 +0000

    Eliminacion de carpetas duplicadas

--- practicaColas/practicaColas/Data-structures-/controller/__init__.py
+++
@@ -1,1 +0,0 @@
-from .ticketController import TicketController
--- practicaColas/practicaColas/Data-structures-/model/node.py
+++
@@ -1,7 +0,0 @@
-from .ticket import Ticket
-
-class Node:
-    def __init__(self, data: Ticket, priority: int) -> None:
-        self.data = data
-        self.priority = priority
-        self.next = None
